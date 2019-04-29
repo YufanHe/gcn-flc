ffe3e9fbe329c5e4cce71dfe13ad5f5af752fa82
Pengfei <lipengfeizju@gmail.com>
2019-04-29 00:02:46 -0400

    fix not feasible case

--- eval_result.py
+++ eval_result.py
@@ -11,24 +11,24 @@ import multiprocessing
 from multiprocessing import Pool
 
 parser = argparse.ArgumentParser(description='data generator')
-parser.add_argument('--eval_file', dest='eval_file', default='./dataset/synthetic/dataset-04-28-09-26-04_output.json', help='hyperparameters')
-parser.add_argument('--result_dir', dest='result_dir', default='./dataset/result/', help='hyperparameters')
-parser.add_argument('--parallel_core', dest='parallel_core', default=3, help='hyperparameters')
+parser.add_argument('--eval_file', dest='eval_file', default='./dataset/output/dataset-04-28-09-26-04_output.json', help='Result file from GCN to be evaluated')
+parser.add_argument('--result_dir', dest='result_dir', default='./dataset/result/', help='Directory to save the evaluation result')
+parser.add_argument('--parallel_core', dest='parallel_core', default=3, type=int, help='Parallel process to evaluate the result')
 
 def main():
     global args
     args = parser.parse_args()
     cfg, data = loaddata(args.eval_file)
     # number of samples
-    # N = cfg['sample_num']
-    N = 10
+    N = cfg['sample_num']
+    # N = 10
     # Add shared data between processes
     data_save=multiprocessing.Manager().list()   
     lock= multiprocessing.Manager().Lock() 
     # Add thread pool 
     p = Pool(args.parallel_core)
     for i in range(N):
-        p.apply_async(eval_multi, args=(data[i],data_save,lock)) 
+        p.apply_async(eval_multi, args=(data[i],data_save,lock,i)) 
     # Wait for all threads finish
     p.close()
     p.join()
@@ -42,9 +42,12 @@ def main():
     # Open file to save
     with open(file_save, 'w') as fp:
         fp.write(json.dumps(list(data_save), indent=3))
+    data_np = np.array(data_save)
+    a = data_np[:,2]
+    a = a[a < 10]
+    print(a.mean())
 
-
-def eval_multi(data_node,data_save,lock):
+def eval_multi(data_node,data_save,lock,idx):
     """
     Multi_process function for result evaluation
     
@@ -88,10 +91,13 @@ def eval_multi(data_node,data_save,lock):
     c1 = np.ma.masked_array(cost_overall, mask=1 - gd_mask)
     gd_cost = gd_dis.data.sum() + c1.sum()
     c2 = np.ma.masked_array(cost_overall, mask=1 - pd_mask)
-    pd_cost = pd_dis.data.sum() + c2.sum()
-
-    print("The optimal cost is %f, ther predicted cost is %f , ratio is %f" \
-          % (gd_cost, pd_cost, pd_cost/gd_cost))
+    if c2.all() is np.ma.masked:
+        print("%03d: Not feasible" %(idx))
+        pd_cost = 1e5
+    else:
+        pd_cost = pd_dis.data.sum() + c2.sum()
+        print("%03d: The optimal cost is %0.2f, ther predicted cost is %0.2f , ratio is %0.2f" \
+          % (idx, gd_cost, pd_cost, pd_cost/gd_cost))
 
     with lock:
         data_save.append([gd_cost, pd_cost, pd_cost/gd_cost])
